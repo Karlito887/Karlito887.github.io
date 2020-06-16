d83e0ac833451dbc7dda8ff2471f80ac98178049
Karlyk Nazarii <karliknazar@gmail.com>
2020-06-17 00:57:45 +0300

    Updates

---
+++ precache-manifest.38fd98bcbd1521e0f19b0bcc08379f14.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db43e7af8ee1d51f1e20fe945616677",
+    "url": "/socnet/index.html"
+  },
+  {
+    "revision": "812d30402097f58f44dd",
+    "url": "/socnet/static/css/3.719ac1c1.chunk.css"
+  },
+  {
+    "revision": "169076c15b0ad0fda280",
+    "url": "/socnet/static/css/4.5ebaa216.chunk.css"
+  },
+  {
+    "revision": "c0a21303ac397dafccf3",
+    "url": "/socnet/static/css/main.c21885e2.chunk.css"
+  },
+  {
+    "revision": "70adf992d7ce71124438",
+    "url": "/socnet/static/js/2.c69897ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socnet/static/js/2.c69897ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812d30402097f58f44dd",
+    "url": "/socnet/static/js/3.f8038185.chunk.js"
+  },
+  {
+    "revision": "169076c15b0ad0fda280",
+    "url": "/socnet/static/js/4.098ac337.chunk.js"
+  },
+  {
+    "revision": "c0a21303ac397dafccf3",
+    "url": "/socnet/static/js/main.64d1cb98.chunk.js"
+  },
+  {
+    "revision": "495af613a8b13faa30bf",
+    "url": "/socnet/static/js/runtime-main.58ac0947.js"
+  }
+]);

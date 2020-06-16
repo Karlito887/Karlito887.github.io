ba14c3632f864db38a90fa00069eb11fb5177f6e
Karlyk Nazarii <karliknazar@gmail.com>
2020-06-16 22:40:02 +0300

    Updates

---
+++ precache-manifest.29aad345287a4152734b1cd3c6afcd44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dec41c0ff4cd4a598aef4b4d398f706",
+    "url": "/Karlitos887.github.io/index.html"
+  },
+  {
+    "revision": "812d30402097f58f44dd",
+    "url": "/Karlitos887.github.io/static/css/3.719ac1c1.chunk.css"
+  },
+  {
+    "revision": "169076c15b0ad0fda280",
+    "url": "/Karlitos887.github.io/static/css/4.5ebaa216.chunk.css"
+  },
+  {
+    "revision": "ab9f1812c060842643aa",
+    "url": "/Karlitos887.github.io/static/css/main.c21885e2.chunk.css"
+  },
+  {
+    "revision": "70adf992d7ce71124438",
+    "url": "/Karlitos887.github.io/static/js/2.c69897ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Karlitos887.github.io/static/js/2.c69897ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812d30402097f58f44dd",
+    "url": "/Karlitos887.github.io/static/js/3.f8038185.chunk.js"
+  },
+  {
+    "revision": "169076c15b0ad0fda280",
+    "url": "/Karlitos887.github.io/static/js/4.098ac337.chunk.js"
+  },
+  {
+    "revision": "ab9f1812c060842643aa",
+    "url": "/Karlitos887.github.io/static/js/main.037360f6.chunk.js"
+  },
+  {
+    "revision": "cc7f3ed4704988647106",
+    "url": "/Karlitos887.github.io/static/js/runtime-main.d1c3175d.js"
+  }
+]);
